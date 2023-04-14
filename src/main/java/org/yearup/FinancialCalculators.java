afcc8dbfb2d31c699d334e8d635ade1b8c132462
Brad <bradegbert26@gmail.com>
2023-04-14 15:05:45 -0500

    getMortgage done

--- src/main/java/org/yearup/FinancialCalculators.java
+++ src/main/java/org/yearup/FinancialCalculators.java
@@ -11,23 +11,34 @@ public class FinancialCalculators
     {
         // Variables
         double principal;
-        double interestRate;
+        double annualIR;
+        double monthlyIR;
         int loanLength;
+        double monthlyPayment;
+        double numPayments;
+        double totalInterest;
 
         // Query user & assign variables
-        System.out.print("Enter your amount: ");
+        System.out.print("Enter loan amount: ");
         principal = scanner.nextDouble();
         scanner.nextLine();
 
-        System.out.print("Enter your interest rate: ");
-        interestRate = scanner.nextDouble();
+        System.out.print("Enter annual interest rate: ");
+        annualIR = scanner.nextDouble() / 100;
         scanner.nextLine();
 
         System.out.print("Enter number of years: ");
         loanLength = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println(principal + " " + interestRate + " " + loanLength);
+        // Calculate
+        numPayments = loanLength * 12;
+        monthlyIR = annualIR / 1200;
+        monthlyPayment = principal * (monthlyIR * Math.pow(1 + monthlyIR, numPayments)) / (Math.pow(1 + monthlyIR, numPayments) - 1);
+        totalInterest = (monthlyPayment * numPayments) - principal;
+
+        System.out.printf("Monthly payment: %.2f$\n", monthlyPayment);
+        System.out.printf("Total interest paid: %.2f$\n",  totalInterest);
     }
 
     public static void main(String[] args)
